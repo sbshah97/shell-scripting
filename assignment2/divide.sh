b2da2ebb88961196040abd6725ec59acd9e89688
student <student@itadmin-ug1>
2016-08-29 16:10:28 +0530

    Sorting question remaining

--- assignment2/divide.sh
+++ assignment2/divide.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-
-if [ $1 -le 0 -o $2 -le 0]
+if [ $2 -le 0 ]
 then
-	echo "Wrong input. Try again."
+echo "Please enter the number again "
+read n1
+div=$(echo "scale=2; $1 / $n1" | bc)
+echo "$div"
 else
-elif[ $1 -le $2 ]
-then
-	echo "$(echo "scale=2; $1 / $2"|bc -l)"
-else
-	echo "$(echo "scale=2; $2 / $1"|bc -l)"
+div=$(echo "scale=2; $1 / $2" | bc)
+echo "$div"
 fi
-
-

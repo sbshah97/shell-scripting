bead2bf25eb7674ef8248c3c2e606df39c08f9b2
student <student@itadmin-ug1>
2016-08-29 14:17:26 +0530

    Assignment 2 fully completed

--- assignment2/divide.sh
+++ assignment2/divide.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-echo "Enter the first number"
-read n
-echo "Enter the second number"
-read n1
-
-echo "$n / ( $n1 - 0 )" | bc -l
+if [ $1 -le 0 -o $2 -le 0]
+	then 
+	echo "Wrong input. Try again."
+else
+	if[ $1 -l $2 ]
+		then 
+		echo "$(echo "scale=2; $1/$2 " | bc)"
+	else 
+		echo "$(echo "scale=2; $2/$1" | bc)"
+	fi
+fi
 
--- assignment2/rectangle.sh
+++ assignment2/rectangle.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 result=0
-echo "Enter length of rectangle"
 read l
-echo "Enter breadth of rectangle"
 read b
 
 result=`expr $l \* $b`
